70ef1763538f1c9ba42156c80e1cb1f0866dcaae
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-15 11:53:39 +0100

    updated urls to be secure

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast{
   constructor(){
     this.key = "g2ufoaoqvClKTLmXxlneEZNHLvKjIRWv"
-    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/'
-    this.cityURI = "http://dataservice.accuweather.com/locations/v1/cities/search"
+    this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/'
+    this.cityURI = "https://dataservice.accuweather.com/locations/v1/cities/search"
   }
 
   async updateCity(city) {
